3ca98467a098363ea20d0974b05f0e79b9c02beb
ridzy <ridzuanazmi.11@gmail.com>
2023-01-20 10:02:48 +0800

    updated some file and .gitignore

--- sdf/src/main/java/nusiss/App.java
+++ sdf/src/main/java/nusiss/App.java
@@ -1,9 +1,7 @@
 package nusiss;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
--- sdf/src/main/java/nusiss/ArraySortExample.java
+++ sdf/src/main/java/nusiss/ArraySortExample.java
@@ -1,7 +1,6 @@
 package nusiss;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 
 public class ArraySortExample {
